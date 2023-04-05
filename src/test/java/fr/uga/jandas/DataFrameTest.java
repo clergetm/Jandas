ffe510206e74f9eb54b13dc21c2d20ec33f8725a
Armand G <g.armand2000@gmail.com>
2023-04-05 11:17:40 +0200

    add test template for DataFrame

---
+++ src/test/java/fr/uga/jandas/DataFrameTest.java
@@ -0,0 +1,37 @@
+package fr.uga.jandas;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataFrameTest {
+
+    @Test
+    void CreateDataFrameInt() {
+
+        // Array    [[ 1 2 3 ]
+        //           [ 4 5 6 ]]
+        int[] col1 = new int[]{1, 4};
+        int[] col2 = new int[]{2, 5};
+        int[] col3 = new int[]{3, 6};
+        // DataFrame
+        // DataFrame df = new DataFrame();
+
+        // test col 1
+        // test col 2
+        // test col 3
+
+        // test ligne 1
+        // test ligne 2
+
+        // test value 1
+        // test value 2
+        // test value 3
+        // test value 4
+        // test value 5
+        // test value 6
+    }
+    @Test
+    void getName() {
+    }
+}
