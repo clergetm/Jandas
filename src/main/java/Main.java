2454fb91fb6690a1a7189fdcfe4aa60c474c8acc
Mathys Clerget <mathysclerget+github@gmail.com>
2023-04-05 09:13:58 +0200

    feat: initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void hello(){
+        System.out.println("Hello, World!");
+
+    }
+    public static void main(String[] args) {
+        hello();
+    }
+}
