777bb1e612aec241742dbd4ac624bd7d71fb1588
davidge <geoffrey.david@univ-grenoble-alpes.fr>
2023-04-26 15:36:09 +0200

    Feat:documentation

--- src/main/java/fr/uga/jandas/Column.java
+++ src/main/java/fr/uga/jandas/Column.java
@@ -1,23 +1,52 @@
 package fr.uga.jandas;
 
-
+/**
+ * The Column class represents a 1-dimension array. It is used by the DataFrame class to store elements in 2-dimensions array.
+ * A Column store one type of object. This type can be : Integer, Float, Double, Boolean or String. Others types are not
+ * supported.
+ *
+ * This class contains an attribute called type used to do calculations and statistics. It is overkill since Column is
+ * already generic, but this is the only way we found to get the elements type.
+ *
+ * @param <T> The type of objects stored in the column
+ */
 @SuppressWarnings("unchecked")
 public class Column<T>{
     String label;
     private String type;
     T [] elements;
 
+    /**
+     * Initialize a newly created Column object so that it represents an empty 1D-array. It will contain
+     * a certain number of objects of defined type.
+     * @param type The type of objects in the column (array).
+     * @param nb_elements The number of objects in the array.
+     */
     public Column(String type, int nb_elements){
         this.type = type;
         elements = (T[]) new Object[nb_elements];
     }
 
+    /**
+     * Initialize a newly created Column object so that it represents a 1D-array given in argument. It contains
+     * a certain number of objects of defined type. Note that the given array is deep copied.
+     * @param type The type of objects in the column (array).
+     * @param nb_elements The number of objects in the array.
+     * @param arr The array with objects to add in column.
+     */
     public Column(String type, int nb_elements, T [] arr){
         this.type = type;
         elements = (T[]) new Object[nb_elements];
         System.arraycopy(arr, 0, elements, 0, nb_elements);
     }
 
+    /**
+     * Same as others constructors but it allows to add the column label directly.
+     * @param l The column label.
+     * @param type The type of objects in the column (array).
+     * @param nb_elements The number of objects in the array.
+     * @param arr The array with objects to add in column.
+     */
     public Column(String l, String type, int nb_elements, T [] arr){
         this.type = type;
         label = l;
@@ -25,6 +54,13 @@ public class Column<T>{
         System.arraycopy(arr, 0, elements, 0, nb_elements);
     }
 
+    /**
+     * Used internally to create a deep copy of Column.
+     * @param l The copied label
+     * @param indexes The indexes of lines to add in the new Column.
+     * @param arr The elements to be copied.
+     * @param t The type of elements.
+     */
     private Column(String l, String t, int [] indexes, T [] arr){
         // We create a copy of elements
         T [] new_elements = (T[]) new Object[arr.length];
@@ -38,33 +74,64 @@ public class Column<T>{
         }
     }
 
+    /**
+     * Set the label of the current Column.
+     * @param l The new label name.
+     */
     public void setLabel(String l){
         label = l;
     }
 
+    /**
+     * Return the label of the current Column.
+     * @return The String representing the label.
+     */
     public String getLabel(){
         return label;
     }
 
+    /**
+     * Used to add an element in the current Column at given index. If there is another element in there,
+     * it will be erased. Note that it suppose that the element has the good type.
+     * @param e The element to add.
+     * @param index The element index.
+     */
     public void addElement(T e, int index){
         elements[index] = e;
     }
 
+    /**
+     * Returns all elements in the current Column on an 1D array.
+     * @return An array filled of element in column.
+     */
     public T[] getElements(){
         return elements;
     }
 
+    /**
+     * Returns the element at given index.
+     * Returns null if the index is out of range.
+     * @param index The element index.
+     * @return The requested element or null if error, or if it not exists.
+     */
     public T getElement(int index){
+        if (index < 0 || index > elements.length)
+            return null;
         return elements[index];
     }
 
+    /**
+     * Returns the number of element in the column.
+     * @return The number of elements.
+     */
     public int getSize(){
         return elements.length;
     }
 
-    /* mean :
-        Function that calculates mean value of column if and only if the column
-        is of type Integer, or Float, or Double. Else, it will return null.
+    /**
+     * Returns mean value of column if and only if the column
+     * is of type Integer, or Float, or Double. Else, it will return null.
+     * @return The mean value computed.
      */
     public Double mean() {
         Double sum = 0.;
@@ -79,19 +146,15 @@ public class Column<T>{
                     sum += ((Float) element).doubleValue();
                 }
                 return sum/elements.length;
-            case "Double":
-                for (T element : elements) {
-                    sum += (Double) element;
-                }
-                return sum/elements.length;
             default:
                 return null;
         }
     }
 
-    /* max :
-        Function that calculates max value of column if and only if the column
-        is of type Integer, or Float, or Double. Else, it will return null.
+    /**
+     * Returns the max value of column if and only if the column
+     * is of type Integer, or Float, or Double. Else, it will return null.
+     * @return The max value computed.
      */
     public Double max(){
         Double max = null;
@@ -108,12 +171,6 @@ public class Column<T>{
                         max = ((Float) element).doubleValue();
                 }
                 return max;
-            case "Double":
-                for (T element : elements) {
-                    if ( max == null || ((Double) element) > max)
-                        max = (Double) element;
-                }
-                return max;
             default:
                 return null;
         }
@@ -122,6 +179,12 @@ public class Column<T>{
     /* min :
         Function that calculates min value of column if and only if the column
         is of type Integer, or Float, or Double. Else, it will return null.
+     */
+
+    /**
+     * Returns the min value of column if and only if the column
+     * is of type Integer, or Float, or Double. Else, it will return null.
+     * @return The min value computed.
      */
     public Double min(){
         Double min = null;
@@ -138,21 +201,30 @@ public class Column<T>{
                         min = ((Float) element).doubleValue();
                 }
                 return min;
-            case "Double":
-                for (T element : elements) {
-                    if ( min == null || ((Double) element) < min)
-                        min = (Double) element;
-                }
-                return min;
             default:
                 return null;
         }
     }
 
+    /**
+     * Returns a partial deep copy of current Column. The newly created Columns contains copies of every element
+     * at given index.
+     * @param indexes The array of indexes of elements in column to be copied in new Column.
+     * @return The newly created Column.
+     */
     public Column<T> createPartialCopy(int [] indexes){
+        for (int i=0; i<indexes.length; i++){
+            if (indexes[i] < 0 || indexes[i] > elements.length) return null;
+        }
         return new Column<>(label, type, indexes, elements);
     }
 
+    /**
+     * Compares this Column to the specified object. The result is true if and only if the argument is not null and is a
+     * Column object that represents the same sequence of elements, and same type, as this object.
+     * @param obj The object to compare this Column against
+     * @return true if the given object represents a Column equivalent to this Column, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (getClass() != obj.getClass()) return false;
--- src/main/java/fr/uga/jandas/DataFrame.java
+++ src/main/java/fr/uga/jandas/DataFrame.java
@@ -2,20 +2,29 @@ package fr.uga.jandas;
 
 import java.io.FileInputStream;
 import java.text.DecimalFormat;
-import java.util.Date;
 import java.util.Objects;
 import java.util.Scanner;
 
 /**
-    Format des fichiers en entrée:
-        t0,     t1,     t2,     t3
-        l0;     l1,     l2,     l3
-        n00,    n01,    n02,    n03
-        n10,    n11,    n12,    n13
-    La première ligne donne les types des colonnes. La seconde contient les étiquettes de chaque colonne.
-    Puis chaque ligne correspond à une ligne du tableau et les virgules correspondent aux séparations
-    entre les colonnes. Les portions de texte séparées par une virgule correspondent ainsi aux contenus des
-    cellules du tableau.
+ * The Dataframe class represents a 2-dimensions array where each column is identified by a label and each line by an index.
+ * Two different columns can host two different types.
+ * <p>
+ * The Dataframe class includes methods for examinating individual or multiple elements of the array. Moreover there are
+ * functions that providing statistics on columns.
+ * <p>
+ * Below the actual file format attend to create a Dataframe from a file.
+ * Input file format with n the number of column and m the number of line :<br>
+ * t0,t1,...,tn<br>
+ * l0,l1,...,ln<br>
+ * e00,e01,...,e0n<br>
+ * e10,e11,...,e1n<br>
+ * ...,...,...,...<br>
+ * em0,em1,...,emn<br>
+ * The first line gives the type of each column. The second line gives the labels of each column. The following lines
+ * give the elements (data) to add in the DataFrame. Each cell is separated by a coma <bold>without space</bold>.
+ *
+ * Columns are stored thanks to Column class, and each Column can store objects sharing the same type.
+ *
  **/
 public class DataFrame {
 
@@ -23,6 +32,11 @@ public class DataFrame {
     int lines;
     String [] labels;
 
+    /**
+     * Initialize a newly created DataFrame object so that it represent the 2D-array given in argument.
+     * Note that labels attribute is initialized with the labels written in columns.
+     * @param columns The array containing the columns with labels initialized.
+     */
     DataFrame(Column [] columns){
         lines = columns[0].getSize();
         this.columns = columns;
@@ -33,6 +47,13 @@ public class DataFrame {
         }
     }
 
+    /**
+     * Initialize a newly created DataFrame object so that it represent the 2D-array given a file name. This file need
+     * to follow the CSV format described on DataFrame documentation. Recall : <br>
+     * The first line gives the type of each column. The second line gives the labels of each column. The following lines
+     * give the elements (data) to add in the DataFrame. Each cell is separated by a coma <bold>without space</bold>.
+     * @param filename The name of the file to create DataFrame from.
+     */
     DataFrame(String filename){
         try {
             // Counting the number of lines
@@ -115,10 +136,20 @@ public class DataFrame {
         }
     }
 
+    /**
+     * Returns the column localized at given index.
+     * @param index The column index.
+     * @return The requested column.
+     */
     public Column getColumn(int index){
         return columns[index];
     }
 
+    /**
+     * Returns the column identified by given label. Returns null if there is not a column labelled with this label.
+     * @param label The column label.
+     * @return The requested column or null.
+     */
     public Column getColumn(String label){
         for (int l=0; l< columns.length; l++){
             if (Objects.equals(columns[l].getLabel(), label)){
@@ -128,8 +159,13 @@ public class DataFrame {
         return null;
     }
 
+    /**
+     * Returns the line identified by given index.
+     * @param index The index of requested line.
+     * @return The object array containing requested elements.
+     */
     public Object[] getLine(int index){
-        if (index > lines)
+        if ( index < 0 || index > lines)
             throw new IndexOutOfBoundsException();
         Object [] chosen_lines = new Object[columns.length];
         for (int i = 0; i < columns.length; i++){
@@ -138,17 +174,30 @@ public class DataFrame {
         return chosen_lines;
     }
 
+    /**
+     * Returns the element localized at given column and lineIndex index. Return null if an index is out of range.
+     * @param colIndex The column index of accessed element.
+     * @param lineIndex The line index of accessed element.
+     * @return The requested element or null.
+     */
     public Object getElement(int colIndex, int lineIndex){
-        return columns[colIndex].getElement(lineIndex);
+        try {
+            return columns[colIndex].getElement(lineIndex);
+        } catch (IndexOutOfBoundsException e){
+            return null;
+        }
     }
 
+    /**
+     * Returns an array containing each labels in order of attribute column array.
+     * @return The labels array
+     */
     public String [] getLabels() {
         return labels;
     }
 
-    /* mean :
-    Prints the mean value calculated of each column. If the mean is not calculable,
-    it prints NaN.
+    /**
+     * Prints the mean value calculated of each column. If the mean is not calculable, it prints NaN instead.
      */
     public void mean(){
         StringBuilder str = new StringBuilder("     ");
@@ -169,9 +218,8 @@ public class DataFrame {
         System.out.println(str);
     }
 
-    /* max :
-    Prints the max value calculated of each column. If the max is not calculable,
-    it prints NaN.
+    /**
+     * Prints the max value calculated of each column. If the max is not calculable, it prints NaN instead.
      */
     public void max(){
         StringBuilder str = new StringBuilder("     ");
@@ -192,9 +240,8 @@ public class DataFrame {
         System.out.println(str);
     }
 
-    /* min :
-    Prints the min value calculated of each column. If the min is not calculable,
-    it prints NaN.
+    /**
+     *  Prints the min value calculated of each column. If the min is not calculable, it prints NaN instead.
      */
     public void min(){
         StringBuilder str = new StringBuilder("     ");
@@ -215,64 +262,96 @@ public class DataFrame {
         System.out.println(str);
     }
 
-
-    /* createFrom :
-        Creates a new Dataframe based on a column set of current Dataframe.
+    /**
+     * Returns a new Dataframe initialized given a column set of current Dataframe. Note that this is a deep copy,
+     * there are new objects used in this newly created DataFrame. Moreover, it can be many times the same label in the given column set.
+     * Returns null a label is unknown.
+     * @param labels The columns label to add in the new DataFrame.
+     * @return The newly created DataFrame or null.
      */
     public DataFrame createFrom(String [] labels){
         Column [] cols = new Column[labels.length];
         for (int l=0; l<labels.length; l++){
-            cols[l] = getColumn(labels[l]);
+            Column c = getColumn(labels[l]);
+            if (c == null) return null;
+            cols[l] = c;
         }
         return new DataFrame(cols);
     }
 
-    /* createFrom :
-    Creates a new Dataframe based on a line set of current Dataframe.
-    It uses createPartialCopy() for every column, a function that returns
-    a copy with only specified lines.
- */
+    /**
+     * Returns a new Dataframe initialized given a line set of current DataFrame. It uses function createPartialCopy()
+     * from the class Column on each column, a function that returns a deep copy with only specified lines. Note that this is a deep copy,
+     * there are new objects used in this newly created DataFrame.
+     * Returns null if an index is out of range.
+     * @param indexes The indexes set to add in the new DataFrame.
+     * @return The newly created DataFrame or null.
+     */
     public DataFrame createFrom(int [] indexes){
         Column [] newCols = new Column[columns.length];
         for (int i=0; i<columns.length; i++){
-            newCols[i] = columns[i].createPartialCopy(indexes);
+            Column c = columns[i].createPartialCopy(indexes);
+            if (c == null) return null;
+            newCols[i] = c;
         }
         return new DataFrame(newCols);
     }
 
-    /* createFrom :
-    Creates a new Dataframe based on a line and column set of current Dataframe.
-    Only the lines of indexes referenced in parameter are present in columns.
+    /**
+     * Returns a new DataFrame initialized given a line and column set of current DataFrame. Only lines of indexes referenced
+     * in parameter are present in obtained columns. Note that this is a deep copy,
+     * there are new objects used in this newly created DataFrame.
+     * Returns null if an index is out of range or a labels is unknown.
+     * @param labels The columns label to add in the new DataFrame.
+     * @param lines The indexes set to add in the new DataFrame.
+     * @return The newly created Dataframe based on the given selection and projection or null.
      */
     public DataFrame createFrom(String [] labels, int [] lines){
         Column [] cols = new Column[labels.length];
         for (int l=0; l<labels.length; l++){
-            cols[l] = getColumn(labels[l]);
+            Column c = getColumn(labels[l]);
+            if (c == null) return null;
+            cols[l] = c;
         }
         Column [] newCols = new Column[labels.length];
         for (int i=0; i<cols.length; i++){
-            newCols[i] = cols[i].createPartialCopy(lines);
+            Column c2 = cols[i].createPartialCopy(lines);
+            newCols[i] = c2;
         }
         return new DataFrame(newCols);
     }
-
 
     private void print(int start, int end){
         System.out.print(toString(start, end));
     }
 
+    /**
+     * Prints the entire DataFrame.
+     */
     public void print(){
         print(0, lines);
     }
 
+    /**
+     * Prints the DataFrame given first lines.
+     * @param nbLines The number of lines to display.
+     */
     public void printFirstLines(int nbLines){
         print(0, nbLines);
     }
 
+    /**
+     * Prints the DataFrame given first lines.
+     * @param nbLines The number of lines to display.
+     */
     public void printLastLines(int nbLines){
         print(lines-nbLines, lines);
     }
 
+    /**
+     * Returns a String object representing this DataFrame's value. Uses print() method.
+     * @return a string representation of the value of this object like an array.
+     */
     @Override
     public String toString(){
         return toString(0, lines);
@@ -296,6 +375,12 @@ public class DataFrame {
         return str.toString();
     }
 
+    /**
+     * Compares this DataFrame to the specified object. The result is true if and only if the argument is not null and is a
+     * DataFrame object that represents the same sequence of elements as this object.
+     * @param obj The object to compare this DataFrame against
+     * @return true if the given object represents a DataFrame equivalent to this DataFrame, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (getClass() != obj.getClass()) return false;
