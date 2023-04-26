388966e4250b9e2f33ce4fe0538f7168d5b32caa
Moutontone <74041699+Moutontone@users.noreply.github.com>
2023-04-26 17:55:42 +0200

    Dataframe (#10)
    Add new Features for dataframes.
    Min, Max and Mean function for Column and for the whole dataFrame.
    
    To present the library jandas a demonstration file called main has been created.
    
    ---------
    
    Co-authored-by: davidge <geoffrey.david@univ-grenoble-alpes.fr>
    Co-authored-by: Armand G <g.armand2000@gmail.com>
    Co-authored-by: Polluxine <geoffrey.david@orange>
    Co-authored-by: Mathys Clerget <mathysclerget+github@gmail.com>

--- src/main/java/fr/uga/jandas/Column.java
+++ src/main/java/fr/uga/jandas/Column.java
@@ -1,30 +1,64 @@
 package fr.uga.jandas;
 
-
+/**
+ * The Column class represents a 1-dimension array. It is used by the DataFrame class to store elements in 2-dimensions array.
+ * A Column store one type of object. This type can be : Integer, Float, Double, Boolean or String. Others types are not
+ * supported.
+ *
+ * This class contains an attribute called type used to do calculations and statistics. It is overkill since Column is
+ * already generic, but this is the only way we found to get the elements type.
+ *
+ * @param <T> The type of objects stored in the column
+ */
 @SuppressWarnings("unchecked")
 public class Column<T>{
     String label;
     private String type;
     T [] elements;
 
+    /**
+     * Initialize a newly created Column object so that it represents an empty 1D-array. It will contain
+     * a certain number of objects of defined type.
+     * @param type The type of objects in the column (array).
+     * @param nb_elements The number of objects in the array.
+     */
     public Column(String type, int nb_elements){
         this.type = type;
         elements = (T[]) new Object[nb_elements];
     }
 
+    /**
+     * Initialize a newly created Column object so that it represents a 1D-array given in argument. It contains
+     * a certain number of objects of defined type. Note that the given array is deep copied.
+     * @param type The type of objects in the column (array).
+     * @param nb_elements The number of objects in the array.
+     * @param arr The array with objects to add in column.
+     */
     public Column(String type, int nb_elements, T [] arr){
         this.type = type;
         elements = (T[]) new Object[nb_elements];
         System.arraycopy(arr, 0, elements, 0, nb_elements);
     }
 
+    /**
+     * Same as others constructors but it allows to add the column label directly.
+     * @param l The column label.
+     * @param type The type of objects in the column (array).
+     * @param nb_elements The number of objects in the array.
+     * @param arr The array with objects to add in column.
+     */
     public Column(String l, String type, int nb_elements, T [] arr){
-        this.type = type;
+        this(type, nb_elements, arr);
         label = l;
-        elements = (T[]) new Object[nb_elements];
-        System.arraycopy(arr, 0, elements, 0, nb_elements);
     }
 
+    /**
+     * Used internally to create a deep copy of Column.
+     * @param l The copied label
+     * @param indexes The indexes of lines to add in the new Column.
+     * @param arr The elements to be copied.
+     * @param t The type of elements.
+     */
     private Column(String l, String t, int [] indexes, T [] arr){
         // We create a copy of elements
         T [] new_elements = (T[]) new Object[arr.length];
@@ -38,42 +72,170 @@ public class Column<T>{
         }
     }
 
+    /**
+     * Set the label of the current Column.
+     * @param l The new label name.
+     */
     public void setLabel(String l){
         label = l;
     }
 
+    /**
+     * Return the label of the current Column.
+     * @return The String representing the label.
+     */
     public String getLabel(){
         return label;
     }
 
+    /**
+     * Used to add an element in the current Column at given index. If there is another element in there,
+     * it will be erased. Note that it suppose that the element has the good type.
+     * @param e The element to add.
+     * @param index The element index.
+     */
     public void addElement(T e, int index){
         elements[index] = e;
     }
 
+    /**
+     * Returns all elements in the current Column on an 1D array.
+     * @return An array filled of element in column.
+     */
     public T[] getElements(){
         return elements;
     }
 
+    /**
+     * Returns the element at given index.
+     * Returns null if the index is out of range.
+     * @param index The element index.
+     * @return The requested element or null if error, or if it not exists.
+     */
     public T getElement(int index){
+        if (index < 0 || index > elements.length)
+            return null;
         return elements[index];
     }
 
+    /**
+     * Returns the number of element in the column.
+     * @return The number of elements.
+     */
     public int getSize(){
         return elements.length;
     }
 
+    /**
+     * Returns mean value of column if and only if the column
+     * is of type Integer, or Float, or Double. Else, it will return null.
+     * @return The mean value computed.
+     */
+    public Double mean() {
+        Double sum = 0.;
+        switch (type){
+            case "Integer":
+                for (T element : elements) {
+                    sum += ((Integer) element).doubleValue();
+                }
+                return sum/elements.length;
+            case "Float":
+                for (T element : elements) {
+                    sum += ((Float) element).doubleValue();
+                }
+                return sum/elements.length;
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Returns the max value of column if and only if the column
+     * is of type Integer, or Float, or Double. Else, it will return null.
+     * @return The max value computed.
+     */
+    public Double max(){
+        Double max = null;
+        switch (type){
+            case "Integer":
+                for (T element : elements) {
+                    if ( max == null || ((Integer) element).doubleValue() > max)
+                        max = ((Integer) element).doubleValue();
+                }
+                return max;
+            case "Float":
+                for (T element : elements) {
+                    if ( max == null || ((Float) element).doubleValue() > max)
+                        max = ((Float) element).doubleValue();
+                }
+                return max;
+            default:
+                return null;
+        }
+    }
+
+    /* min :
+        Function that calculates min value of column if and only if the column
+        is of type Integer, or Float, or Double. Else, it will return null.
+     */
+
+    /**
+     * Returns the min value of column if and only if the column
+     * is of type Integer, or Float, or Double. Else, it will return null.
+     * @return The min value computed.
+     */
+    public Double min(){
+        Double min = null;
+        switch (type){
+            case "Integer":
+                for (T element : elements) {
+                    if ( min == null || ((Integer) element).doubleValue() < min)
+                        min = ((Integer) element).doubleValue();
+                }
+                return min;
+            case "Float":
+                for (T element : elements) {
+                    if ( min == null || ((Float) element).doubleValue() < min)
+                        min = ((Float) element).doubleValue();
+                }
+                return min;
+            default:
+                return null;
+        }
+    }
+
+    /**
+     * Returns a partial deep copy of current Column. The newly created Columns contains copies of every element
+     * at given index.
+     * @param indexes The array of indexes of elements in column to be copied in new Column.
+     * @return The newly created Column.
+     */
     public Column<T> createPartialCopy(int [] indexes){
+        for (int i=0; i<indexes.length; i++){
+            if (indexes[i] < 0 || indexes[i] > elements.length) return null;
+        }
         return new Column<>(label, type, indexes, elements);
     }
 
+    /**
+     * Compares this Column to the specified object. The result is true if and only if the argument is not null and is a
+     * Column object that represents the same sequence of elements, and same type, as this object.
+     * If there are empty elements in a column, equals return false.
+     * @param obj The object to compare this Column against
+     * @return true if the given object represents a Column equivalent to this Column, false otherwise
+     */
     @Override
     public boolean equals(Object obj) {
         if (getClass() != obj.getClass()) return false;
         Column col = (Column) obj;
         if (col.getSize() != getSize()) return false;
         if (!col.type.equals(type)) return false;
+        if (col.label == null && label != null) return false;
+        if (col.label != null && label == null) return false;
+        if (label != null && !col.label.equals(label)) return false;
         boolean res = true;
         for (int i = 0; i < col.getSize() ;i++){
+            if (col.getElement(i) == null || getElement(i) == null) return false;
             res = res && col.getElement(i).equals(getElement(i));
         }
         return res;
--- src/test/java/fr/uga/jandas/ColumnTest.java
+++ src/test/java/fr/uga/jandas/ColumnTest.java
@@ -3,6 +3,8 @@ package fr.uga.jandas;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.text.DecimalFormat;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ColumnTest {
@@ -71,5 +73,78 @@ class ColumnTest {
         assertNotEquals(col, colbis);
         col.addElement(100, 4);
         assertEquals(col, colbis);
+    }
+
+    @Test
+    void TestSetLabel(){
+        Float[] arr = {1.F, 2.F, 2.9F, 3.9F, 4.8F, 5.5F};
+        Column<Float> c = new Column<>("ratio", "Float", 6, arr);
+        Column<Float> c2 = new Column<>("Float", 6, arr);
+        assertNotEquals(c, c2);
+        c2.setLabel("ratio");
+        assertEquals(c, c2);
+        c.setLabel("flower");
+        assertNotEquals(c, c2);
+    }
+
+    @Test
+    void ColumnLowArg(){
+        Float[] arr = {1.F, 2.F, 2.9F, 3.9F, 4.8F, 5.5F};
+        Column<Float> c = new Column<>("Float", 6, arr);
+        Column<Float> c2 = new Column<>("Float", 6);
+        assertNotEquals(c, c2);
+        for (int i = 0; i < arr.length; i++){
+            c2.addElement(arr[i], i);
+        }
+        assertEquals(c, c2);
+    }
+
+    @Test
+    void TestMax(){
+        Float[] arr = {1.F, 2.F, 2.9F, 3.9F, 4.8F, 5.5F};
+        Column<Float> c = new Column<>("ratio", "Float", 6, arr);
+        assertEquals(5.5F, c.max());
+        Integer[] arri = {1, 33, 29, 39, 8, 5};
+        Column<Integer> c2 = new Column<>("ratio", "Integer", 6, arri);
+        assertEquals(39, c2.max());
+        String[] str = {"aeaea", "sjdfhsf"};
+        Column<String> c3 = new Column<>("ratio", "String", 2, str);
+        assertNull(c3.max());
+        Boolean[] b = {false, true, false, false};
+        Column<Boolean> c4 = new Column<>("ratio", "Boolean", 4, b);
+        assertNull(c4.max());
+    }
+
+    @Test
+    void TestMin(){
+        Float[] arr = {1.F, 2.F, 2.9F, 3.9F, 4.8F, 5.5F};
+        Column<Float> c = new Column<>("ratio", "Float", 6, arr);
+        assertEquals(1.F, c.min());
+        Integer[] arri = {1, 33, 29, 39, 8, 5};
+        Column<Integer> c2 = new Column<>("ratio", "Integer", 6, arri);
+        assertEquals(1, c2.min());
+        String[] str = {"aeaea", "sjdfhsf"};
+        Column<String> c3 = new Column<>("ratio", "String", 2, str);
+        assertNull(c3.min());
+        Boolean[] b = {false, true, false, false};
+        Column<Boolean> c4 = new Column<>("ratio", "Boolean", 4, b);
+        assertNull(c4.min());
+    }
+
+    @Test
+    void TestMean(){
+        DecimalFormat decf = new DecimalFormat("0.#####");
+        Float[] arr = {1.F, 2.F, 2.9F, 3.9F, 4.8F, 5.5F};
+        Column<Float> c = new Column<>("ratio", "Float", 6, arr);
+        assertEquals("3.35", decf.format(c.mean()));
+        Integer[] arri = {1, 33, 29, 39, 8, 5};
+        Column<Integer> c2 = new Column<>("ratio", "Integer", 6, arri);
+        assertEquals(19.166666666666668, c2.mean());
+        String[] str = {"aeaea", "sjdfhsf"};
+        Column<String> c3 = new Column<>("ratio", "String", 2, str);
+        assertNull(c3.mean());
+        Boolean[] b = {false, true, false, false};
+        Column<Boolean> c4 = new Column<>("ratio", "Boolean", 4, b);
+        assertNull(c4.mean());
     }
 }
